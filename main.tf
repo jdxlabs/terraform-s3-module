8d84e65c7919cc60882cb8ac17e26a72e6986d22
Jérôme Devoucoux <jdevoucoux@gmail.com>
2023-12-08 17:23:07 +0100

    wip

---
+++ main.tf
@@ -0,0 +1,6 @@
+
+resource "aws_s3_bucket" "b" {
+    bucket = var.name
+    acl    = "private"
+    tags = var.tags
+}
---
+++ variables.tf
@@ -0,0 +1,12 @@
+
+variable "name" {
+  type        = string
+  default     = null
+  description = "Name for the bucket. If omitted, Terraform will assign a random, unique name."
+}
+
+variable "tags" {
+  type = object()
+  default     = null
+  description = "Details the tags to apply to the bucket"
+}
